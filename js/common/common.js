01611dce40b8e302cd3805e40e7e3de1346168e3
HieuPham2000 <hieu.pt183535@gmail.com>
2021-07-05 13:45:08 +0700

    update: test load data

--- js/common/common.js
+++ js/common/common.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  alert("Hello World!");
+  loadData();
 })
+
+function loadData(a) {
+  
+  // Lấy dữ liệu về
+  $.ajax({
+    url: "http://cukcuk.manhnv.net/v1/Employees",
+    method: "GET",
+  }).done(function(res) {
+    var data = res;
+    debugger
+  }).fail(function(res) {
+  })
+  // binding dữ liệu lên table
+}
