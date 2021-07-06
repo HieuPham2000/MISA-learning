f99dd64fd5cffe6d58255f703ae357e84ded8134
HieuPham2000 <hieu.pt183535@gmail.com>
2021-07-06 17:33:46 +0700

    add: add some files

--- js/base/main.js
+++ js/base/main.js
@@ -1,10 +1,16 @@
 $(document).ready( function() {
+  toggleMenu();
+})
 
+/**-------------------------------
+ * Mở rộng/ thu hẹp sidebar (menu)
+ * Author: pthieu (03/07/2021)
+ */
+function toggleMenu() {
   $(".header-left > .sidebar").click( function() {
     var toggleWidth = $(".menu").width() == 225 ? "51px" : "225px";
     var pageLeft = $(".menu").width() == 225 ? "52px" : "226px";
     $(".menu").animate({width: toggleWidth});
     $(".page").animate({left: pageLeft});
-    // $(".header-left").css("border-bottom", "1px solid #dedede");
   })
-})
+}
