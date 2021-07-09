994f81ee527e044dd4717356352099bb10cb8359
HieuPham2000 <hieu.pt183535@gmail.com>
2021-07-10 06:26:26 +0700

    fix: combobox load data from api

--- js/common/combobox.js
+++ js/common/combobox.js
@@ -1,7 +1,6 @@
-$(document).ready(function() {
-  console.log("hêllo")
-  new Combobox("#combobox-department", "cukcuk.manhnv.net", "api/Department", "Department");
-  new Combobox("#combobox-position", "cukcuk.manhnv.net/v1", "Positions", "Position");
+$(document).ready(function () {
+  new Combobox(".combobox-container[data-name=Department]", "cukcuk.manhnv.net", "api/Department", "Department");
+  new Combobox(".combobox-container[data-name=Position]", "cukcuk.manhnv.net/v1", "Positions", "Position");
   makeCombobox();
 })
 
@@ -30,12 +29,17 @@ class Combobox {
     }).done(function (response) {
       let data = response;
       let list = $(container).children(".dropdown");
-      $.each(data, function(index, item) {
+      $.each(data, function (index, item) {
         var propertyName = me.Name + "Name";
         var li = `<li><i class="fas fa-check"></i>${item[propertyName]}</li>`;
         list.append(li);
-      })
-      
+      });
+
+
+
+
+      selectItemInDropdown();
+
     }).fail(function (response) {
       console.log(response)
       ToastMessage.add("danger", "Tải dữ liệu thất bại!");
@@ -46,34 +50,40 @@ class Combobox {
 
 
 function makeCombobox() {
-  
+
   // Ẩn tất cả dropdown khi click ra ngoài
-  $(document).click(function(event) {
+  $(document).click(function (event) {
     var el = event.target;
     // console.log(el)
-    if( $(".combobox-container").has(el).length == 0 && !$(".combobox-container").is(el)) {
+    if ($(".combobox-container").has(el).length == 0 && !$(".combobox-container").is(el)) {
       // console.log($(".combobox-container").has(el))
       closeAllDropdown();
     }
   })
 
-  $(".combobox-container button").click(function() {
+  $(".combobox-container button").click(function () {
     toggleDropdownByButton.call(this);
   })
 
-  $(".selectbox input").click(function() {
+  $(".selectbox input").click(function () {
     toggleDropdownByButton.call(this);
   })
 
-  $(".combobox-container input:not([readonly])").on('input', function() {
+  $(".combobox-container input:not([readonly])").on('input', function () {
     var container = $(this).parents(".combobox-container");
     openDropdown(container);
-    var value = $(this).val().toLowerCase();
-    container.find("li").each(function() {
-      if($(this).text().toLowerCase().indexOf(value) > -1) {
+    var value = CommonFunction.nonAccentVietnamese($(this).val()).toLowerCase();
+    container.find("li").each(function () {
+      var text = CommonFunction.nonAccentVietnamese($(this).text()).toLowerCase();
+      if (text.indexOf(value) > -1) {
         $(this).show();
       } else {
         $(this).hide();
+      }
+      if(text == value) {
+        $(this).addClass("selected");
+      } else {
+        $(this).removeClass("selected");
       }
     })
   })
@@ -91,7 +101,7 @@ function closeAllDropdown() {
 function toggleDropdownByButton() {
   // $(this).parents(".combobox-container").toggleClass("open-dropdown");
   var container = $(this).parents(".combobox-container");
-  if(container.is(".open-dropdown")) {
+  if (container.is(".open-dropdown")) {
     container.removeClass("open-dropdown");
   } else {
     container.find("li").show();
@@ -102,7 +112,19 @@ function toggleDropdownByButton() {
 function openDropdown(container) {
   closeAllDropdown();
   container.addClass("open-dropdown");
-  if(!container.find("input").is("[readonly]")) {
+  var value = container.find(".combobox-input input").val();
+  value =  CommonFunction.nonAccentVietnamese(value).toLowerCase();
+  console.log(value)
+  var lis = container.find(".combobox-dropdown li");
+  $.each(lis, function(index, item) {
+    var text = CommonFunction.nonAccentVietnamese($(item).text()).toLowerCase();
+      if(text == value) {
+        $(item).addClass("selected");
+      } else {
+        $(item).removeClass("selected");
+      }
+  })
+  if (!container.find("input").is("[readonly]")) {
     container.children(".combobox-input").css("border", "1px solid #019160");
   }
 
@@ -116,7 +138,7 @@ function openDropdownByInput() {
  * 
  */
 function selectItemInDropdown() {
-  $(".combobox-dropdown li").click(function() {
+  $(".combobox-dropdown li").click(function () {
     var container = $(this).parents(".combobox-container");
     $(this).siblings(".selected").removeClass("selected");
     $(this).addClass("selected");
@@ -124,7 +146,7 @@ function selectItemInDropdown() {
     container.removeClass("open-dropdown");
 
     var input = container.find("input");
-    input.val( $(this).text() );
+    input.val($(this).text());
     input.siblings(".btn-clear-text").show();
 
   })
